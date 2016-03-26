7ec61f4ac735477c0f0350a9e3fc98017cddfbc1
trevormkay <trevormkay@me.com>
2016-03-26 22:35:42 +1100

    Added a working navbar.
    Includes styling on desktop as well as mobile functionality.

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	
+	$( ".hamburger" ).click(function() {
+	  $( ".hamburger" ).toggleClass( "displayed" );
+	  $( ".cover" ).toggleClass( "displayed" );
+	  $( ".categories" ).toggleClass( "displayed" );
+	});
+	
+});
