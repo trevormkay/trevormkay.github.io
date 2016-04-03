6a805984c5594ca4f7e9ec40c0e23c067d8d0998
trevormkay <trevormkay@me.com>
2016-04-03 15:16:24 +1000

    Added smooth scrolling

--- app.js
+++ app.js
@@ -4,4 +4,19 @@ $(document).ready(function(){
 	  $( ".hamburger, .cover, .categories" ).toggleClass( "displayed" );
 	});
 	
+	
+	$('a[href^="#"]').click(function(e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+	
+				
 });
