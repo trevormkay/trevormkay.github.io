d226d660af66931f2fc0b19cc189e2792a1bef92
trevormkay <trevormkay@me.com>
2016-04-09 18:17:09 +1000

    Started adding image content for the Epoch page
    Still need to add some images, style the dynamic countdown, and make
    everything properly responsive.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
 	
+	
+	//MOBILE MENU TOGGLE
 	$( ".hamburger, .cover" ).click(function() {
 	  $( ".hamburger, .cover, .categories" ).toggleClass( "displayed" );
 	});
 	
 	
+	//SCROLL TO
 	$('a[href^="#"]').click(function(e) {
 	    e.preventDefault();
 
@@ -17,6 +20,50 @@ $(document).ready(function(){
 	        window.location.hash = target;
 	    });
 	});
+	
+	
+	//COUNTDOWN
+	function getTimeRemaining(endtime) {
+	  var t = Date.parse(endtime) - Date.parse(new Date());
+	  var seconds = Math.floor((t / 1000) % 60);
+	  var minutes = Math.floor((t / 1000 / 60) % 60);
+	  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	  return {
+	    'total': t,
+	    'days': days,
+	    'hours': hours,
+	    'minutes': minutes,
+	    'seconds': seconds
+	  };
+	}
+	
+	function initializeClock(id, endtime) {
+	  var clock = document.getElementById(id);
+	  var daysSpan = clock.querySelector('.days');
+	  var hoursSpan = clock.querySelector('.hours');
+	  var minutesSpan = clock.querySelector('.minutes');
+	  var secondsSpan = clock.querySelector('.seconds');
+	
+	  function updateClock() {
+	    var t = getTimeRemaining(endtime);
+	
+	    daysSpan.innerHTML = t.days;
+	    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+	    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+	    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+	
+	    if (t.total <= 0) {
+	      clearInterval(timeinterval);
+	    }
+	  }
+	
+	  updateClock();
+	  var timeinterval = setInterval(updateClock, 1000);
+	}
+	
+	var deadline = 'December 25 2016';
+	initializeClock('clockdiv', deadline);	
 	
 				
 });
